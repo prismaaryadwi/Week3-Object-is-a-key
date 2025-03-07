3b2e50d4bfab22848560e42dbe2404559c828655
prismaaryadwi <prisma.aryadwi@gmail.com>
2025-03-07 19:03:20 +0700

    soal5

---
+++ quiz/part1/soal5.js
@@ -0,0 +1,26 @@
+//sistem ubah hurufnya misal huruf a diubah menjadi b, c menjadi d, b menjadi c, z menjadi a
+//intinya ubah huruf menjadi huruf setelahnya
+function ubahHuruf(kata) {
+    let hasil = '';
+
+    for (let i = 0; i < kata.length; i++) {
+        let charCode = kata.charCodeAt(i); //mengambil kode ASCII tiap huruf
+        let nextChar = String.fromCharCode(charCode + 1); //mengubah ke huruf berikutnya
+
+        // jika hurug ada 'z', ubah ke 'a'
+        if (kata[i] === 'z') {
+            nextChar = 'a';
+        }
+
+        hasil += nextChar; //menyusn hasil akhir
+    }
+
+    return hasil;
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
